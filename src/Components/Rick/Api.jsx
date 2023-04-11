2f59707068061c83ef948ec5a20d5c3ad9e117c3
Sagul-09 <nihal.evil.royal@gmail.com>
2023-04-11 09:27:54 +0530

    api-changes

--- src/Components/Rick/Api.jsx
+++ src/Components/Rick/Api.jsx
@@ -1,13 +1,13 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./api.css";
 
 function App() {
   const [Data, setData] = useState([]);
 
   useEffect(() => {
     axios
-      .get("https://rickandmortyapi.com/api/character")
+      .get("https://rickandmortyapi.com/api/character/?count=100")
       .then((response) => {
         setData(response.data.results);
         console.log(response.data.results);
@@ -18,15 +18,19 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>Rick and Morty</h1>
+    <div className="api">
+      <div className="title">
+        <h1>Rick and Morty</h1>
+      </div>
       {Data?.map((character) => (
         <div key={character.id}>
-          <img src={character.image} alt="" />
-          <p>Name: {character.name}</p>
-          <p>Gender: {character.gender}</p>
-          <p>Status: {character.status}</p>
-          <p>Species: {character.species}</p>
+          <div className="charBox">
+            <img src={character.image} alt="" />
+            <p>Name: {character.name}</p>
+            <p>Gender: {character.gender}</p>
+            <p>Status: {character.status}</p>
+            <p>Species: {character.species}</p>
+          </div>
         </div>
       ))}
     </div>
